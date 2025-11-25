4958039b22182ae89d37613e460ef1019470138a
adicto55 <12759@holbertonstudents.com>
2025-11-25 21:36:22 +0000

    update

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -7,8 +7,3 @@ def print_last_digit(number):
         remainder=(10-number%10)%10
     print(remainder,end='')
     return remainder
-    
-print_last_digit(98)
-print_last_digit(0)
-r = print_last_digit(-1024)
-print(r)
