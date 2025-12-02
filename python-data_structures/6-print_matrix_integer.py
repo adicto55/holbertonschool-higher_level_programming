1132ba2fc2a0c826b281b8bcadbd83af545b4ece
dadaw <rzawd@gmail.com>
2025-12-02 18:55:47 +0000

    asda

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,4 +2,4 @@
 def print_matrix_integer(matrix=[[]]):
     for i in range(0,len(matrix)):
         for j in range(0,len(matrix[i])):
-            print("{:d}".format(matrix[i][j]),end=" ")
+            print(" ".join("{:d}".format(matrix[i][j])))
