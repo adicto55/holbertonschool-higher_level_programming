f5058546c6804f6f875ba6ac6e01012c59e36f23
adicto55 <your-email@example.com>
2026-01-04 22:32:00 +0400

    ds

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -4,7 +4,7 @@
 
 class MyList(list):
     """Class that inherits from list with a print_sorted method"""
-    
+
     def print_sorted(self):
         """Print the list sorted in ascending order"""
         print(sorted(self))
--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -4,11 +4,11 @@
 
 class BaseGeometry:
     """A base geometry class with area and integer_validator methods"""
-    
+
     def area(self):
         """Raise an exception - area() is not implemented"""
         raise Exception("area() is not implemented")
-    
+
     def integer_validator(self, name, value):
         """Validate that value is an integer greater than 0"""
         if type(value) is not int or type(value) is bool:
