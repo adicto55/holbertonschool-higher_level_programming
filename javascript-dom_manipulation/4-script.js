63ae95cf53fe325c0bcf1f4740b455082fa26469
adicto55 <your-email@example.com>
2026-02-21 12:55:18 +0400

    ds

--- javascript-dom_manipulation/4-script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('add_item').addEventListener('click', function() {
-  var newItem = document.createElement('li');
-  newItem.textContent = 'Item';
-  document.querySelector('ul.my_list').appendChild(newItem);
-});
--- javascript-dom_manipulation/8-script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  fetch('https://hellosalut.stefanbohacek.com/?lang=fr')
-    .then(response => response.json())
-    .then(data => {
-      document.getElementById('hello').textContent = data.hello;
-    });
-});
